c710bbfa32ed9c7b606449fafca7ae407bc1d252
Fedor <090301fidr@gmail.com>
2020-11-04 18:42:51 +0000

    change some code in demon

--- linux/processing/demon/demon.c
+++ linux/processing/demon/demon.c
@@ -11,8 +11,8 @@
 
 void print_shit_and_pid(int fd, int pid)
 {
-	char shit[30];
-	sprintf(shit, "Shit!(also pid = %d)\n", pid); 
+	char shit[60];
+	sprintf(shit, "Shit!(also pid = %d and session leader pid = %d)\n", pid, getsid(0)); 
 	write(fd, shit, strlen(shit));
 }
 
@@ -37,26 +37,29 @@ int main(int c, char **val)
 		printf("Syscall 'fork' return error with number: %d!\n", pid);
 		exit(-1);
 	}
-	else if (pid == 0)
+	else if (pid == 0) // child process
 	{
 		umask(0);
-		setsid();
-		//chdir("/");
-		log = open(val[1], O_WRONLY|O_CREAT);
+		char *filename = val[1];
+		int new_pid = setsid();
+		log = open(filename, O_WRONLY | O_CREAT);
+		printf("Childprocess can write into parent files!\n");
 		if (log < 0)
 		{
 			exit(-1);
 		}
 		close(STDIN_FILENO);
-		close(STDIN_FILENO);
-		close(STDIN_FILENO);
-		print_shit_and_pid(log, pid);
+		close(STDOUT_FILENO);
+		close(STDERR_FILENO);
+		print_shit_and_pid(log, new_pid);
 		close(log);
 		return 0;
 	}
-	else 
+	else // parent process
 	{
-		printf("Main process has ended!\n");
+		sleep(3);
+		wait(NULL);
+		printf("Main process(pid = %d) has ended, child process pid = %d, session leader pid = %d!\n", getpid(), pid, getsid(0));
 		return 0;
 	}
 }
